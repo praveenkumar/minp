35c16bbdbfef3dbd6061305738ffa7d1a530c528
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2025-02-20 14:19:06 +0530

    Update the minp script which use for linux

--- minp.sh
+++ minp.sh
@@ -2,12 +2,11 @@
 
 set -exuo pipefail
 
-podman run --hostname 127.0.0.1.nip.io --detach --rm -it --privileged -p 9080:80 -p 9443:443 -p 6443:6443 \
-    -v $(pwd)/00-dns.yaml:/etc/microshift/config.d/00-dns.yaml \
-    -v $(pwd)/storage.conf:/etc/containers/storage.conf \
+sudo podman run --hostname 127.0.0.1.nip.io --detach --rm -it --privileged -p 9080:80 -p 9443:443 -p 6443:6443 \
     -v /var/lib/containers/storage:/host-container:ro,rshared \
-    --name microshift quay.io/praveenkumar/microshift-okd:flannel-arm64
+    --name microshift quay.io/praveenkumar/microshift-okd:4.18.0-okd-scos.1-amd64
 
 sleep 20
 
-podman cp  microshift:/var/lib/microshift/resources/kubeadmin/127.0.0.1.nip.io/kubeconfig .
+sudo podman cp  microshift:/var/lib/microshift/resources/kubeadmin/127.0.0.1.nip.io/kubeconfig .
+sudo chown $USER:$USER kubeconfig
